652561d11b29769232760c63789761df8cce605a
Osmar Pereira <bibliotecaosmar@gmail.com>
2019-09-16 20:38:21 -0300

    Funcionalidades completadas

--- js/functions.js
+++ js/functions.js
@@ -3,6 +3,10 @@
  *  Utilities
  * =======================================
  */
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 function sequencialIntNoRepeated(rang, arr = []) {
   x = Math.floor(Math.random() * 10).toString()
   if(arr.indexOf(x) == -1 && x < rang) {
@@ -21,6 +25,29 @@ function setCards(a) {
   }
 }
 
+//============================================//
+function flip(card_num) {
+  card = document.getElementById('item-'+card_num).className
+  cardsFlippeds = parseInt(document.getElementById('FlippedCards').innerHTML)
+  if(card.indexOf('hidden_') != -1) {
+    document.getElementById("item-"+card_num).className = removeHidden(card)
+    ++cardsFlippeds
+    document.getElementById('FlippedCards').innerHTML = cardsFlippeds.toString()
+  }
+  checkFlips()
+}
+
+function checkFlips() {
+  if(document.getElementById('FlippedCards').innerHTML == 2) {
+    if(comparator()) {
+      lockCards()
+    }else {
+      sleep(1000)
+      resetChance()
+    }
+  }
+}
+
 function removeHidden(card) {
   return card.split('_')[1]
 }
@@ -29,17 +56,52 @@ function addHidden(card) {
   return ['hidden_', card].join('')
 }
 
-function flipLimiter() {
-
+function addLocked(card) {
+  return ['locked_', card].join('')
 }
 
 function comparator() {
-
+  let cards = []
+  for(let i = 0; i < 6; i++) {
+    card = document.getElementById('item-'+(i+1)).className
+    if(card.indexOf('hidden_') == -1 && card.indexOf('locked_') == -1) {
+      cards.push(card)
+    }
+  }
+  if(cards[0] == cards[1]) {
+    return true
+  }
+  return false
 }
+
+// function applyCards() {
+
+// }
 
 function lockCards() {
-
+  for(let i = 0; i < 6; i++) {
+    card = document.getElementById('item-'+(i+1)).className
+    if(card.indexOf('hidden_') == -1 && card.indexOf('locked_') == -1) {
+      document.getElementById('item-'+(i+1)).className = addLocked(card)
+    }
+  }
+  document.getElementById('FlippedCards').innerHTML = "0"
 }
+
+function setCardsNotLockeds() {
+  for(let i = 0; i < 6; i++) {
+    card = document.getElementById('item-'+(i+1)).className
+    if(card.indexOf('hidden_') == -1 && card.indexOf('locked_') == -1) {
+      document.getElementById('item-'+(i+1)).className = addHidden(card)
+    }
+  }
+}
+
+function resetChance() {
+  setCardsNotLockeds()
+  document.getElementById('FlippedCards').innerHTML = "0"
+}
+
 
 /**
  * =======================================
@@ -65,55 +127,25 @@ function shuffle() {
  */
 
 function flipCard1() {
-  let card = document.getElementById("item-1").className
-  if(card.indexOf('hidden_') != -1){
-    document.getElementById("item-1").className = removeHidden(card)
-  }else{
-    document.getElementById("item-1").className = addHidden(card)
-  }
+  flip(1)
 }
 
 function flipCard2() {
-  let card = document.getElementById("item-2").className
-  if(card.indexOf('hidden_') != -1){
-    document.getElementById("item-2").className = removeHidden(card)
-  }else{
-    document.getElementById("item-2").className = addHidden(card)
-  }
+  flip(2)
 }
 
 function flipCard3() {
-  let card = document.getElementById("item-3").className
-  if(card.indexOf('hidden_') != -1){
-    document.getElementById("item-3").className = removeHidden(card)
-  }else{
-    document.getElementById("item-3").className = addHidden(card)
-  }
+  flip(3)
 }
 
 function flipCard4() {
-  let card = document.getElementById("item-4").className
-  if(card.indexOf('hidden_') != -1){
-    document.getElementById("item-4").className = removeHidden(card)
-  }else{
-    document.getElementById("item-4").className = addHidden(card)
-  }
+  flip(4)
 }
 
 function flipCard5() {
-  let card = document.getElementById("item-5").className
-  if(card.indexOf('hidden_') != -1){
-    document.getElementById("item-5").className = removeHidden(card)
-  }else{
-    document.getElementById("item-5").className = addHidden(card)
-  }
+  flip(5)
 }
 
 function flipCard6() {
-  let card = document.getElementById("item-6").className
-  if(card.indexOf('hidden_') != -1){
-    document.getElementById("item-6").className = removeHidden(card)
-  }else{
-    document.getElementById("item-6").className = addHidden(card)
-  }
+  flip(6)
 }
